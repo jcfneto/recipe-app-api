23a017870ed59e738f198a6406ad8235d9fdfd0c
jcfneto <eng.jcfneto@gmail.com>
2022-02-03 19:20:02 -0300

    Configure postgres db.

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,11 @@ MAINTAINER Jose Carlos Ferreira Neto
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /requirements.txt
+RUN apk add --update --no-cache postgresql-client
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+		gcc libc-dev linux-headers postgresql-dev
 RUN pip install -r requirements.txt
+RUN apk del .tmp-build-deps
 
 RUN mkdir /app
 WORKDIR /app
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,23 @@
+from unittest.mock import patch
+
+from django.test import TestCase
+from django.db.utils import OperationalError
+from django.core.management import call_command
+
+
+class CommandsTestCase(TestCase):
+
+    def test_wait_for_db_ready(self):
+        """Test waiting for db when db is available"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 1)
+
+    @patch('time.sleep', return_value=None)
+    def test_wait_for_db(self, ts):
+        """Test waiting for db"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 6)	
