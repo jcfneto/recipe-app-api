7dcb703ab255349c1d5fcc72b3d265f4ecd92595
jcfneto <eng.jcfneto@gmail.com>
2022-02-04 17:52:00 -0300

    Add me endpoint.

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import CreateUserView, CreateTokenView
+from .views import CreateUserView, CreateTokenView, ManageUserView
 
 
 app_name = 'user'
@@ -8,4 +8,5 @@ app_name = 'user'
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
     path('token/', CreateTokenView.as_view(), name='token'),
+    path('me/', ManageUserView.as_view(), name='me'),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,6 @@
-from rest_framework import generics
 from rest_framework.settings import api_settings
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework import generics, authentication, permissions
 
 from .serializers import UserSerializer, AuthTokenSerializer
 
@@ -14,3 +14,14 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user."""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authentication user."""
+        return self.request.user
