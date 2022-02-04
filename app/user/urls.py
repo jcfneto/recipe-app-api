49249d2c248ace05f067a95576334c068edcde97
jcfneto <eng.jcfneto@gmail.com>
2022-02-04 08:20:11 -0300

    Tests from api.

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import CreateUserView
+from .views import CreateUserView, CreateTokenView
 
 
 app_name = 'user'
 
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
+    path('token/', CreateTokenView.as_view(), name='token'),
 ]
