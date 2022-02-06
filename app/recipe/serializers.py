4cfcd66c0a8db93e418e3c9e2f7c4a4a811f8b53
jcfneto <eng.jcfneto@gmail.com>
2022-02-06 16:11:36 -0300

    Added custom ecipe detail view.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -44,3 +44,9 @@ class RecipeSerializer(serializers.ModelSerializer):
             'link'
         )
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer a recipe detail."""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
